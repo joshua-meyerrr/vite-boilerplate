6a2ae13a46395362de0a8f818c769ac311d75b4a
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-16 12:20:10 +0200

    ✨ Create basic express server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('Hello Server!');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (request, response) => {
+  response.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
